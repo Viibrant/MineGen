f9517478c69c0384604286cbda32a00c0103c00d
Viibrant <11376396+Viibrant@users.noreply.github.com>
2023-02-22 19:12:04 +0000

    Improved dataloader

--- data/datasets/mcs/schematic.py
+++ data/datasets/mcs/schematic.py
@@ -1,5 +1,7 @@
 from nbtschematic import SchematicFile
 from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
+import torch
 from .util import main
 import pandas as pd
 import numpy as np
@@ -8,9 +10,10 @@ import numpy as np
 class SchematicDataset(Dataset):
     def __init__(
         self,
+        threshold: int = 128,
         schematics_dir: str = "schematics",
         transform=None,
-        metadata_file: str = "metadata.csv",
+        metadata_file: str = "data.csv",
         download=False,
         **kwargs
     ):
@@ -22,16 +25,30 @@ class SchematicDataset(Dataset):
 
         self.metadata = pd.read_csv(metadata_file)
 
+        if threshold:
+            self.metadata = self.metadata[self.metadata["X"] < threshold]
+            self.metadata = self.metadata[self.metadata["Y"] < threshold]
+            self.metadata = self.metadata[self.metadata["Z"] < threshold]
+
+        self.threshold = (threshold, threshold, threshold)
+
     def __len__(self):
         return len(self.metadata)
 
-    def __getitem__(self, idx):
-        metadata = self.metadata.iloc[idx]
+    def __getitem__(self, idx, metadata=False):
+        if isinstance(idx, slice):
+            start, stop, step = idx.indices(len(self))
+            return [self[i] for i in range(start, stop, step)]
+
+        metadata = self.metadata.iloc[idx].to_dict()
         sf = SchematicFile.load(metadata["Path"])
+        sf = np.array(sf.blocks)
 
         if self.transform:
-            sf = self.transform(np.array(sf.blocks))
+            sf = self.transform(sf)
 
-        sample = {"data": sf, "metadata": metadata}
-
-        return sample
+        # Pad to threshold
+        sf = np.pad(
+            sf, [(0, self.threshold[i] - sf.shape[i]) for i in range(len(sf.shape))]
+        )
+        return torch.Tensor(sf)
