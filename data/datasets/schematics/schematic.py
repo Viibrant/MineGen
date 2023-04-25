f5cf24c80204239cf4eb136725edcbaea0b9be42
Viibrant <11376396+Viibrant@users.noreply.github.com>
2023-04-25 18:06:00 +0100

    Added collate function

--- data/datasets/schematics/schematic.py
+++ data/datasets/schematics/schematic.py
@@ -1,11 +1,10 @@
-from fastapi import Path
 from .scraper import main
 from pathlib import PurePosixPath
 from nbtschematic import SchematicFile
 from sklearn.preprocessing import OneHotEncoder
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 
-# import lightning.pytorch as pl
+import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import pandas as pd
@@ -56,62 +55,91 @@ class SchematicDataset(Dataset):
         ).toarray()
 
         # Load schematic and convert to numpy array
-        sf = SchematicFile.load(path)
-        sf = np.array(sf.blocks)
+        # TODO! Hacky error handling
+        while True:
+            try:
+                sf = SchematicFile.load(path)
+                sf = torch.abs(torch.tensor(sf.blocks).long())
+                break
+            except Exception as e:
+                # Sample a different schematic
+                return self.__getitem__(
+                    np.random.randint(0, len(self)), return_metadata=return_metadata
+                )
 
         if self.transform:
             sf = self.transform(sf)
 
-        # Pad to threshold
-        sf = np.pad(
-            sf, [(0, self.threshold[i] - sf.shape[i]) for i in range(len(sf.shape))]
-        )
-
         if return_metadata is True:
-            return torch.Tensor(sf), category, metadata
+            return sf, category, metadata
 
-        return torch.Tensor(sf), category
+        return sf, category
+
+
+def custom_collate_fn(batch, size=16):
+    # extract the tensors and categories from the batch
+    batch_sf, batch_category = zip(*batch)
+
+    # compute the maximum shape of the tensors in batch_sf
+    padded_sf = torch.stack(
+        [
+            torch.from_numpy(
+                np.pad(
+                    sf,
+                    [(0, size - sf.shape[i]) for i in range(len(sf.shape))],
+                )
+            )
+            for sf in batch_sf
+        ]
+    )
+
+    batch_category = torch.cat([torch.from_numpy(cat) for cat in batch_category])
+
+    # return padded_sf, torch.stack(batch_category)
+    return padded_sf, batch_category
 
 
 class SchematicDataModule(pl.LightningDataModule):
-    def __init__(
-        self,
-        batch_size: int = 32,
-        threshold: int = 128,
-        data_dir: str = "schematics",
-        download=False,
-        **kwargs
-    ):
+    def __init__(self, batch_size: int = 64, num_workers: int = 4, **dataset_kwargs):
         super().__init__()
         self.batch_size = batch_size
-        self.threshold = threshold
-        self.data_dir = data_dir
-        self.download = download
-        self.kwargs = kwargs
+        self.num_workers = num_workers
+        self.dataset_kwargs = dataset_kwargs
 
-    def prepare_data(self) -> None:
-        if self.download:
-            main()
+    def setup(self, stage=None):
+        if stage in (None, "fit"):
+            full_dataset = SchematicDataset(**self.dataset_kwargs)
+            print(len(full_dataset))
+            train_size = int(0.8 * len(full_dataset))
+            val_size = len(full_dataset) - train_size
+            self.train_dataset, self.val_dataset = random_split(
+                full_dataset, [train_size, val_size]
+            )
 
-    def setup(self, stage: str = ""):
-        """
-        TODO: `stage` here can be used for fit/test/predict stages.
-        """
-        self.dataset = SchematicDataset(
-            threshold=self.threshold, schematics_dir=self.data_dir, **self.kwargs
-        )
+        if stage in (None, "test"):
+            self.test_dataset = SchematicDataset(**self.dataset_kwargs)
 
     def train_dataloader(self):
         return DataLoader(
-            self.dataset, batch_size=self.batch_size, shuffle=True, num_workers=4
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            collate_fn=custom_collate_fn,
         )
 
     def val_dataloader(self):
         return DataLoader(
-            self.dataset, batch_size=self.batch_size, shuffle=False, num_workers=4
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=custom_collate_fn,
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.dataset, batch_size=self.batch_size, shuffle=False, num_workers=4
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=custom_collate_fn,
         )
