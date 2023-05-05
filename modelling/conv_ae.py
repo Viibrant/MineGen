e33b7753ef18ca4d7c4d76686bd2d5db3f998520
Viibrant <11376396+Viibrant@users.noreply.github.com>
2023-05-05 21:56:51 +0100

    Autoencoder implementations

--- modelling/conv_ae.py
+++ modelling/conv_ae.py
@@ -1,64 +1,67 @@
-"""
-Convolutional Autoencoder for voxel data, with a classification head. 
-Uses a 3D convolutional encoder, followed by a 1D linear layer, followed by a 3D convolutional decoder.
-"""
-
+from torch.utils.data import DataLoader, Subset
 from torch import nn
+from tqdm import tqdm
 
 
-class AutoEncoder(nn.Module):
-    def _conv_layer(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.Conv3d(
-                in_channels,
-                out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-            ),
-            nn.LeakyReLU(inplace=True),
-            nn.BatchNorm3d(out_channels),
-            nn.MaxPool3d(2),
-        )
-
-    def _linear_layer(self, in_features, out_features):
-        return nn.Sequential(
-            nn.Linear(in_features, out_features),
-            nn.BatchNorm1d(out_features),
-            nn.LeakyReLU(inplace=True),
-        )
-
-    def __init__(self):
-        super(AutoEncoder, self).__init__()
+class VanillaAutoencoder(nn.Module):
+    def __init__(self, embedding_dim):
+        super(VanillaAutoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            self._conv_layer(1, 64, 3, 2, 1),
-            self._conv_layer(64, 128, 3, 2, 1),  # 128, 8, 8, 8
-            nn.Flatten(),
-            nn.Linear(65536, 128),
-            nn.BatchNorm1d(128),
-            nn.LeakyReLU(inplace=True),
+            nn.Conv3d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(256, embedding_dim, kernel_size=3, stride=2, padding=1),
         )
-
-        self.classifier = nn.Sequential(nn.Linear(128, 19), nn.Softmax(dim=1))
 
         self.decoder = nn.Sequential(
-            nn.Linear(128, 65536),
-            nn.LeakyReLU(inplace=True),
-            nn.BatchNorm1d(65536),
-            nn.Unflatten(1, (128, 8, 8, 8)),
-            nn.ConvTranspose3d(128, 64, kernel_size=3, stride=3, padding=1),
-            nn.LeakyReLU(inplace=True),
-            nn.ConvTranspose3d(64, 32, kernel_size=3, stride=3, padding=1),
-            nn.LeakyReLU(inplace=True),
-            nn.ConvTranspose3d(32, 1, kernel_size=2, stride=2),
-            nn.LeakyReLU(inplace=True),
-            nn.Linear(128, 128),
-            nn.LeakyReLU(inplace=True),
-            nn.BatchNorm3d(1),
+            nn.ConvTranspose3d(embedding_dim, 256, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(32, 16, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose3d(16, 1, kernel_size=3, stride=1, padding=1),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        y_hat = self.classifier(encoded)
-        decoded = self.decoder(encoded)
-        return y_hat, decoded
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat, z
+
+
+def train_voxel_embedding(embedding_dim, num_epochs, batch_size, learning_rate, device):
+    # dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    dataloader = DataLoader(
+        Subset(dataset, range(20)), batch_size=batch_size, shuffle=True, pin_memory=True
+    )
+
+    model = VanillaAutoencoder(embedding_dim).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.MSELoss()
+
+    for epoch in range(num_epochs):
+        epoch_loss = 0
+        for batch in tqdm(sub):
+            batch = batch.unsqueeze(1).float().to(device) / 255.0
+            optimizer.zero_grad()
+            x_hat, z = model(batch)
+            loss = criterion(x_hat, batch)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item() * batch.size(0)
+
+        print(f"Epoch {epoch}: Loss={epoch_loss}")
+
+    return model
